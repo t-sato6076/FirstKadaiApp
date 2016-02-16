e12ae93c5585cf82c543b1aea541a70613972a98
tatsuya sato <tatsuya@sato-no-macbook-air.local>
2016-02-16 21:14:28 +0900

    再提出します

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //再提出のため変更
     }
 
     override func didReceiveMemoryWarning() {
